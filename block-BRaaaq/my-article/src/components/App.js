881a4285a55aad4e6e28b34d6d039a5263bdaf4f
Abhijeet Mane <abhi@Varshas-MacBook-Air.local>
2022-03-17 12:29:04 +0530

    Added Assignment on propTypes in React

---
+++ block-BRaaaq/my-article/src/components/App.js
@@ -0,0 +1,15 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Hero from "./Hero";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Hero/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App;
---
+++ block-BRaaaq/my-article/src/components/Header.js
@@ -0,0 +1,14 @@
+function Header() {
+  return (
+    <header className="flex space-btw">
+      <a className="logo" href="#">Logo</a>
+      <ul className="nav flex">
+        <li> <a href="#">Home</a></li>
+        <li> <a href="#">About</a></li>
+        <li> <a href="#">Contact</a></li>
+      </ul>
+    </header>
+  )
+}
+
+export default Header;
